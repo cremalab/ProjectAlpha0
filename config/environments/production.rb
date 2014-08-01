cd93b4180cfc336e4334242fcb067aa39341492c
matao <mattowens11@gmail.com>
2014-08-01 17:23:23 -0500

    Fix thing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,9 +28,6 @@ Rails.application.configure do
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
 
-
-  config.assets.compile = true
-
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = NoCompression.new
   config.assets.css_compressor = NoCompression.new
